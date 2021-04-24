c7af098fcfc4e9df4219f33ef0f07b9e84b71b00
itisfeature <gall.anonimb@gmail.com>
2021-04-24 23:13:08 +0200

    Setup spring project. Closes #1

---
+++ src/main/java/com/github/programmer0121/sudokuapp/SudokuAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.programmer0121.sudokuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/programmer0121/sudokuapp/SudokuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.programmer0121.sudokuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
