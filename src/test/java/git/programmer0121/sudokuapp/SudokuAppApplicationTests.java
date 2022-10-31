3867d9ef683a588b9eda0fae6779991beaaee426
xlukasz <lukiaplikacje@gmail.com>
2022-10-31 18:32:03 +0100

    upgrade configuration to java17

--- src/test/java/git/programmer0121/sudokuapp/SudokuAppApplicationTests.java
+++ src/test/java/git/programmer0121/sudokuapp/SudokuAppApplicationTests.java
@@ -2,12 +2,18 @@ package git.programmer0121.sudokuapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
 class SudokuAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Configuration
+	public static class Conf {
+		
 	}
 
 }
